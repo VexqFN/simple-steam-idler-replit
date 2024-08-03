b6ac0b633bd5f95873e20e65676d952714a3e677
VexqFN <153433423+VexqFN@users.noreply.github.com>
2024-08-03 11:38:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
+var games = [714010, 824270, 730, 271590, 739630, 431960, 440, 2696480];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
